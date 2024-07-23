49d3cc6faf4c2c2ab6d7cc3df74489fdf473f9d1
PS <pramilanarawade70@gmail.com>
2024-07-23 11:54:15 +0530

    Update buildJavaApp.groovy

--- vars/buildJavaApp.groovy
+++ vars/buildJavaApp.groovy
@@ -1,1 +1,40 @@
-
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Checkout') {
+                steps {
+                    checkout scm
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh 'mvn clean install'
+                }
+            }
+            stage('Test') {
+                steps {
+                    sh 'mvn test'
+                }
+            }
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        def image = docker.build("pramila188/testhello:${env.BUILD_ID}")
+                        image.push()
+                    }
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    sh 'kubectl apply -f deployment.yaml'
+                }
+            }
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
