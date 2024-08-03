a8fb2a0cdbaedc8b5e063f30e5494edc21ca8fe7
PS <pramilanarawade70@gmail.com>
2024-08-03 16:38:55 +0530

    Update sharedPipeline.groovy

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -1,94 +1,120 @@
-pipeline {
-    agent any
-    environment {
-        DOCKERHUB_CREDENTIALS = credentials('dockerhunpwd')
-        SLACK_CREDENTIALS = credentials('jen-slack-pwd')
-    }
-    parameters {
-        string(name: 'GIT_BRANCH', defaultValue: 'main', description: 'Git branch to build')
-    }
-    stages {
-        stage('Clone Repositories') {
-            parallel {
-                stage('Clone Java Application') {
-                    steps {
-                        git branch: "${params.GIT_BRANCH}", url: 'https://github.com/pramilasawant/testhello.git'
-                    }
-                }
-                stage('Clone Python Application') {
-                    steps {
-                        git branch: "${params.GIT_BRANCH}", url: 'https://github.com/pramilasawant/phython-application.git'
-                    }
-                }
-            }
+def call() {
+    pipeline {
+        agent any
+
+        environment {
+            DOCKERHUB_CREDENTIALS = credentials('dockerhubpwd')
+            SLACK_CREDENTIALS = credentials('b3ee302b-e782-4d8e-ba83-7fa591d43205')
         }
-        stage('Build Docker Images') {
-            parallel {
-                stage('Build Java Image') {
-                    steps {
-                        script {
-                            dockerImage = docker.build("pramila188/testhello")
-                        }
-                    }
-                }
-                stage('Build Python Image') {
-                    steps {
-                        script {
-                            dockerImage = docker.build("pramila188/python-app")
-                        }
-                    }
-                }
-            }
+
+        parameters {
+            string(name: 'JAVA_REPO', defaultValue: 'https://github.com/pramilasawant/testhello.git', description: 'Java Application Repository')
+            string(name: 'PYTHON_REPO', defaultValue: 'https://github.com/pramilasawant/phython-application.git', description: 'Python Application Repository')
+            string(name: 'DOCKERHUB_USERNAME', defaultValue: 'pramila188', description: 'DockerHub Username')
+            string(name: 'JAVA_IMAGE_NAME', defaultValue: 'testhello', description: 'Java Docker Image Name')
+            string(name: 'PYTHON_IMAGE_NAME', defaultValue: 'python-app', description: 'Python Docker Image Name')
+            string(name: 'JAVA_NAMESPACE', defaultValue: 'test', description: 'Kubernetes Namespace for Java Application')
+            string(name: 'PYTHON_NAMESPACE', defaultValue: 'python', description: 'Kubernetes Namespace for Python Application')
         }
-        stage('Push Docker Images') {
-            parallel {
-                stage('Push Java Image') {
-                    steps {
-                        script {
-                            docker.withRegistry('', 'dockerhunpwd') {
-                                dockerImage.push("${env.BUILD_NUMBER}")
-                                dockerImage.push("latest")
-                            }
+
+        stages {
+            stage('Clone Repositories') {
+                parallel {
+                    stage('Clone Java Repo') {
+                        steps {
+                            git url: params.JAVA_REPO, branch: 'main'
                         }
                     }
-                }
-                stage('Push Python Image') {
-                    steps {
-                        script {
-                            docker.withRegistry('', 'dockerhunpwd') {
-                                dockerImage.push("${env.BUILD_NUMBER}")
-                                dockerImage.push("latest")
+                    stage('Clone Python Repo') {
+                        steps {
+                            dir('python-app') {
+                                git url: params.PYTHON_REPO, branch: 'main'
                             }
                         }
                     }
                 }
             }
-        }
-        stage('Deploy to Kubernetes') {
-            parallel {
-                stage('Deploy Java Application') {
-                    steps {
-                        script {
-                            sh "helm upgrade --install java-app ./helm/java --set image.repository=pramila188/testhello --set image.tag=${env.BUILD_NUMBER} --namespace test1"
+
+            stage('Build and Push Docker Images') {
+                parallel {
+                    stage('Build and Push Java Image') {
+                        steps {
+                            dir('testhello') { // Ensure this directory contains the pom.xml
+                                sh 'mvn clean install'
+                                script {
+                                    def image = docker.build("${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number}")
+                                    docker.withRegistry('', 'dockerhubpwd') {
+                                        image.push()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    stage('Build and Push Python Image') {
+                        steps {
+                            dir('python-app') {
+                                script {
+                                    def image = docker.build("${params.DOCKERHUB_USERNAME}/${params.PYTHON_IMAGE_NAME}:${currentBuild.number}")
+                                    docker.withRegistry('', 'dockerhubpwd') {
+                                        image.push()
+                                    }
+                                }
+                            }
                         }
                     }
                 }
-                stage('Deploy Python Application') {
-                    steps {
-                        script {
-                            sh "helm upgrade --install python-app ./helm/python --set image.repository=pramila188/python-app --set image.tag=${env.BUILD_NUMBER} --namespace python"
+            }
+
+            stage('Get Approval') {
+                steps {
+                    script {
+                        input message: 'Do you approve this deployment?', ok: 'Yes, deploy'
+                    }
+                }
+            }
+
+            stage('Deploy to Kubernetes with helm') {
+                parallel {
+                    stage('Deploy Java Application with Helm') {
+                        steps {
+                            script {
+                                sh "helm upgrade --install java-app ./helm/java --set image.repository=${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME} --set image.tag=${currentBuild.number} --namespace ${params.JAVA_NAMESPACE}"
+                            }
+                        }
+                    }
+                    stage('Deploy Python Application with Helm') {
+                        steps {
+                            script {
+                                sh "helm upgrade --install python-app ./helm/python --set image.repository=${params.DOCKERHUB_USERNAME}/${params.PYTHON_IMAGE_NAME} --set image.tag=${currentBuild.number} --namespace ${params.PYTHON_NAMESPACE}"
+                            }
                         }
                     }
                 }
             }
         }
-    }
-    post {
-        success {
-            slackSend (color: '#00FF00', message: "SUCCESS: ${env.JOB_NAME} #${env.BUILD_NUMBER}")
-        }
-        failure {
-            slackSend (color: '#FF0000', message: "FAILURE: ${env.JOB_NAME} #${env.BUILD_NUMBER}")
+
+        post {
+            always {
+                script {
+                    def slackBaseUrl = 'https://slack.com/api/'
+                    def slackChannel = '#builds'
+                    def slackColor = currentBuild.currentResult == 'SUCCESS' ? 'good' : 'danger'
+                    def slackMessage = "Build ${currentBuild.fullDisplayName} finished with status: ${currentBuild.currentResult}"
+
+                    echo "Sending Slack notification to ${slackChannel} with message: ${slackMessage}"
+
+                    slackSend (
+                        baseUrl: 'https://yourteam.slack.com/api/',
+                        teamDomain: 'StarAppleInfotech',
+                        channel: '#builds',
+                        color: slackColor,
+                        botUser: true,
+                        tokenCredentialId: 'b3ee302b-e782-4d8e-ba83-7fa591d43205',
+                        notifyCommitters: false,
+                        message: "Build Final_project #${env.BUILD_NUMBER} finished with status: ${currentBuild.currentResult}"
+                    )
+                }
+            }
         }
     }
 }
