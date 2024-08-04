87d55dbe1d97fcb4083957c027ba57f808d17ee8
PS <pramilanarawade70@gmail.com>
2024-08-04 17:43:47 +0530

    Update sharedPipeline.groovy

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -113,13 +113,7 @@ def call() {
                         notifyCommitters: false,
                         message: "Build Final_project #${env.BUILD_NUMBER} finished with status: ${currentBuild.currentResult}"
                     )
-                    emailext (
-                         to: 'pramilanarawade70@gmail.com',
-                         subject: "NOTIFICATION: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' Completed",
-                         body: """<p>Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' completed.</p>
-                         <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a></p>"""
-                         mimeType: 'text/html'
-                        )
+                   
                 }
             }
         }
