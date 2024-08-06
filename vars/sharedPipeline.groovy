06b297d1bce721adefd9ee64e4dc9786eaea06cc
PS <pramilanarawade70@gmail.com>
2024-08-06 13:00:15 +0530

    Update sharedPipeline.groovy

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -121,20 +121,10 @@ def call() {
                 steps {
                     script {
                         withCredentials([file(credentialsId: 'k8spwd', variable: 'KUBECONFIG')]) {
-                            def javaHelmRelease = 'testhello'
-                            def javaHelmChartDir = './myspringbootchart'
-                            def javaNamespace = params.JAVA_NAMESPACE
-
-                            try {
-                                sh """
-                                    helm upgrade --install ${javaHelmRelease} ${javaHelmChartDir} --namespace ${javaNamespace} --create-namespace
-                                """
-                                echo "Java application deployed successfully."
-                            } catch (Exception e) {
-                                echo "Failed to deploy Java application: ${e.message}"
-                                currentBuild.result = 'FAILURE'
-                                error("Deployment failed.")
-                            }
+                            sh '''
+                                export KUBECONFIG=$KUBECONFIG
+                                helm upgrade --install testhello ./myspringbootchart --namespace ${params.JAVA_NAMESPACE} --create-namespace
+                            '''
                         }
                     }
                 }
@@ -144,20 +134,10 @@ def call() {
                 steps {
                     script {
                         withCredentials([file(credentialsId: 'k8spwd', variable: 'KUBECONFIG')]) {
-                            def pythonHelmRelease = 'python-app'
-                            def pythonHelmChartDir = './my-python-app'
-                            def pythonNamespace = params.PYTHON_NAMESPACE
-
-                            try {
-                                sh """
-                                    helm upgrade --install ${pythonHelmRelease} ${pythonHelmChartDir} --namespace ${pythonNamespace} --create-namespace
-                                """
-                                echo "Python application deployed successfully."
-                            } catch (Exception e) {
-                                echo "Failed to deploy Python application: ${e.message}"
-                                currentBuild.result = 'FAILURE'
-                                error("Deployment failed.")
-                            }
+                            sh '''
+                                export KUBECONFIG=$KUBECONFIG
+                                helm upgrade --install python-app ./my-python-app --namespace ${params.PYTHON_NAMESPACE} --create-namespace
+                            '''
                         }
                     }
                 }
